370b44aa28ec1ae6695c2b432ea91665d6d90f31
cnn <carlos.novo@ammeon.com>
2016-10-24 19:54:49 +0100

    minor changes

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # BtlxAutoURL
+
+Bitelxux Oct-2016
+
+
+This is a plugin for trac that automatically expands
+words defined in section [btlxautourl] from trac.ini
+to the URLs defined.
+
+e.g.
+
+[btlxautourl]
+google = http://www.google.com
+starwars = http://www.starwars.com
+
+Then, all the words "google" and "starwars" will be
+rendered automatically as their URLs. Not need any
+more of typing [[http://bla.ble.blu|text]]
+
--- btlxautourl/btlxautourl.py
+++ btlxautourl/btlxautourl.py
@@ -1,7 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-btlxautoexpand.py
+btlxautourl.py
 Bitelxux Oct-2016
+
+
+This is a plugin for trac that automatically expands
+words defined in section [btlxautourl] from trac.ini
+to the URLs defined.
+
+e.g.
+
+[btlxautourl]
+google = http://www.google.com
+starwars = http://www.starwars.com
+
+Then, all the words "google" and "starwars" will be
+rendered automatically as their URLs. Not need any
+more of typing [[http://bla.ble.blu|text]]
+
 """
 
 from genshi.builder import tag
